0dea650e7204094559927d675281850c5d5629a8
Razal <razalbinkabeer01@gmail.com>
2024-09-24 07:55:30 +0400

    Completed Building the header

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import About from "./pages/About";
 import Signout from "./pages/Signout";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/sign-in" element={<Signin />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/signout" element={<Signout />} />
