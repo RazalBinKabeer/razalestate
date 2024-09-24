b40a844be6fdf3d7c1f62e7360c3549b4512715b
Razal <razalbinkabeer01@gmail.com>
2024-09-24 07:37:28 +0400

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Signout from "./pages/Signout";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+
 const App = () => {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signout" element={<Signout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
